60d2686cf92a72d51da090bf5039435a0ce18158
Kahlil Sassa <kahlilsassa@gmail.co>
2022-04-04 20:00:45 -0500

    apicalls 1.0

--- src/components/apiCalls.js
+++ src/components/apiCalls.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const loginCall = async (userCredential, dispatch, ) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post(`${process.env.REACT_APP_BACKEND_SERVER}/auth/login`, 
+    const res = await axios.post(`${process.env.REACT_APP_BACKEND_SERVER}auth/login`, 
     userCredential, 
      {headers: {
       "Content-Type": "application/json", credentials: "include",
